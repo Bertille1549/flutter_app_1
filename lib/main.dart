6178cc8f3061ed5bfa2195b9b6e441454010e8c0
Bertille <pineaubertille1@gmail.com>
2023-09-26 11:14:02 +0200

    ajout d'un bouton0 + img + modif conf flutter

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
         useMaterial3: true,
+        //primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Découverte de Flutter'),
     );
@@ -38,9 +40,15 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-    void _decrementCounter() {
+  void _decrementCounter() {
     setState(() {
       _counter--;
+    });
+  }
+
+    void _resetCounter() {
+    setState(() {
+      _counter = 0;
     });
   }
 
@@ -56,6 +64,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Image.asset('images/fond écran supergirl 1.jpg'),
             const Text(
               'Nombre :',
             ),
@@ -73,14 +82,26 @@ class _MyHomePageState extends State<MyHomePage> {
               alignment: Alignment.bottomLeft,
               child: FloatingActionButton(
                 onPressed: _decrementCounter,
-                child: Icon(Icons.remove_outlined),),
+                child: Icon(Icons.remove_outlined),), //Bouton au centre qui enlève 1
             ),
           ),
-          Align(
-            alignment: Alignment.bottomRight,
-            child: FloatingActionButton(
-              onPressed: _incrementCounter,
-              child: Icon(Icons.add_outlined),),
+
+          Padding(padding: EdgeInsets.only(left:31),
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: FloatingActionButton(
+                onPressed: _incrementCounter,
+                child: Icon(Icons.add_outlined),), //Bouton au centre qui ajoute 1
+            ),
+          ),
+
+          Padding(padding: EdgeInsets.only(left:31),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: FloatingActionButton(
+                onPressed: _resetCounter,
+                child: Icon(Icons.exposure_zero),), //Bouton au centre qui remet à zéro
+            ),
           ),
         ]
       ),
