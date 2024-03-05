3211d30b257bb0f0048af7638b3746c764f31632
Bertille <pineaubertille1@gmail.com>
2024-03-05 19:20:59 +0100

    partie 5 du tp2 fini et fonctionnelle

--- lib/screens/nextpage.dart
+++ lib/screens/nextpage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
+//import 'dart:math';
 
 class Nextpage extends StatefulWidget {
   const Nextpage({Key? key}) : super(key: key);
@@ -11,7 +11,10 @@ class Nextpage extends StatefulWidget {
 class _NextPage extends State<Nextpage> {
   int random = 0;
   String textValue = "";
+  List<String> listeDeValeur = List.generate(20, (index) => (index + 1).toString());
+  int _valueDropDown = 1; // Initialisation de l'index sélectionné à 1
 
+  // fonction qui permet d'afficher un toast
   void _showToast(BuildContext context, String _message) {
     final scaffold = ScaffoldMessenger.of(context);
     scaffold.showSnackBar(
@@ -21,10 +24,12 @@ class _NextPage extends State<Nextpage> {
     );
   }
 
+  // fonction qui permet de savoir à quoi ressemblera le toast
+  // et dans quelles conditions
   void _validateResult() {
     // ajouter ici la logique pour vérifier le résultat saisi
     // et afficher le toast en conséquence
-    if (textValue.isNotEmpty && int.tryParse(textValue) == random) {
+    if (textValue.isNotEmpty && int.tryParse(textValue) == random && _valueDropDown == random) {
       _showToast(context, 'Bravo, le résultat est correct !');
     }
     else {
@@ -45,14 +50,45 @@ color: Colors.green,
             ),
             */
             Padding(padding: EdgeInsets.only(top: 225.0), child: 
-              Align(alignment: Alignment.center, /*heightFactor: 10,*/ child:
+              Align(alignment: Alignment.center, 
+              // ce qui permet d'afficher le nombre
+              /*child:
               Text(
                 '$random',
                 style: Theme.of(context).textTheme.headlineMedium,
               ),
+              */
             ), 
             ),
             
+
+            // ajout du DropDownButton
+            DropdownButton(
+              value: _valueDropDown.toString(),
+              items: listeDeValeur.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              onChanged: (newValue) {
+                setState(() {
+                  _valueDropDown = int.parse(newValue!);
+                  random = _valueDropDown;
+                });
+              },
+              icon: Icon(Icons.audiotrack),
+              //icon: Icon(Icons.arrow_downward), // Ajout d'un icône
+              // Change le style du texte
+              // ajustement de la longueur du padding
+              style: TextStyle(
+                color: Colors.deepPurple,
+              ),
+              dropdownColor: Colors.lightGreen,
+              padding: EdgeInsets.symmetric(horizontal: 10),
+            ),
+
+
             TextButton(
               child: const Text('Générer un nombre aléatoire'),
               style: TextButton.styleFrom(
@@ -62,16 +98,23 @@ color: Colors.green,
                 textStyle: const TextStyle(fontSize: 16),
                 backgroundColor: Colors.blueGrey,
               ),
-              
               onPressed: () {
+                setState(() {
+                  //int intValue = Random().nextInt(20);
+                  //random = intValue;
+
+                });
+                /*
                 // Ajoutez ici la logique pour générer un nombre aléatoire.
                 setState(() {
-                  int intValue = Random().nextInt(50);
+                  int intValue = Random().nextInt(20);
                   random = intValue;
                 });
+                */
               },
-              
             ),
+
+
             TextField(
               keyboardType: TextInputType.number,
               maxLength: 25,
